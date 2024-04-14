b3fa26b4d15307deee8be46c005f519bd7a65be3
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-04-15 00:20:45 +0530

    Update socketServer.ts

--- apps/backend/src/socketServer.ts
+++ apps/backend/src/socketServer.ts
@@ -31,7 +31,7 @@ const OnlineUsersSchema = new mongoose.Schema({
 const OnlineUser = mongoose.model("OnlineUser", OnlineUsersSchema, "onlineUsers");
 
 const app = express();
-const port = process.env.RENDER_WEB_SOCKET_PORT || 5000;
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -46,33 +46,33 @@ const io = new Server(server, {
   },
 })
 
-let rooms: { [key: string]: string[] } = {};
+let rooms = {};
 
 io.on('connection', (socket) => {
   try {
-    socket.on('send_RoomId', (data: { roomId: string, email: string, sender: string, receiver: string }) => {
+    socket.on('send_RoomId', (data) => {
       const { roomId, email, sender, receiver } = data;
       socket.broadcast.emit('check_RoomId', { room_Id: roomId, email, sender, receiver });
     });
 
-    socket.on('online', (data: any) => {
+    socket.on('online', (data) => {
       console.log('user connected', data)
       socket.broadcast.emit('onlineUsers', data);
     });
 
-    socket.on('remove_online', (data: any) => {
+    socket.on('remove_online', (data) => {
       socket.broadcast.emit('remove_online', data);
     })
 
-    socket.on('new_user_message', (data: any) => {
+    socket.on('new_user_message', (data) => {
       socket.broadcast.emit('is_new_user_message', data);
     })
 
-    socket.on('voice_message', (data: any) => {
+    socket.on('voice_message', (data) => {
       socket.broadcast.emit('receive_voice_message', data)
     })
 
-    socket.on('send_Message', (data: { message: string, sender: string, receiver: string, email: string, room_Id: string }) => {
+    socket.on('send_Message', (data) => {
       socket.broadcast.emit('receive_Message', {
         message: data.message,
         sender: data.sender,
@@ -80,11 +80,9 @@ io.on('connection', (socket) => {
         email: data.email,
         roomId: data.room_Id,
       });
-      // console.log('current members in room', rooms[data.room_Id], 'message from', data.user, 'message', data.message)
-      // console.log(rooms)
     });
 
-    socket.on('private_send_Message', (data: { message: string, sender: string, receiver: string, email: string, room_Id: string }) => {
+    socket.on('private_send_Message', (data) => {
       socket.to(data.room_Id).emit('receive_Message', {
         message: data.message,
         sender: data.sender,
@@ -92,16 +90,14 @@ io.on('connection', (socket) => {
         email: data.email,
         roomId: data.room_Id,
       });
-      // console.log('current members in room', rooms[data.room_Id], 'message from', data.user, 'message', data.message)
-      // console.log(rooms)
     });
 
-    socket.on('send_grp_message', (data: any) => {
+    socket.on('send_grp_message', (data) => {
       console.log(data)
       socket.broadcast.emit('receive_grp_message', data)
     })
 
-    socket.on('join_Room', (data: { room_Id: string, username: string }) => {
+    socket.on('join_Room', (data) => {
       if (data) {
         const { room_Id, username } = data
         if (!rooms[room_Id]) {
@@ -115,7 +111,7 @@ io.on('connection', (socket) => {
       }
     });
 
-    socket.on('leave_Room', (data: { room_Id: string, username: string }) => {
+    socket.on('leave_Room', (data) => {
       const { room_Id, username } = data
       if (rooms[room_Id] === undefined) return
       rooms[room_Id] = rooms[room_Id].filter(user => user !== username)
@@ -124,7 +120,7 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', async () => {
       try {
-        const username = socket?.handshake?.auth?.token as string
+        const username = socket?.handshake?.auth?.token
         await connect()
         const user = await OnlineUser.findOne({});
         if(!user) return
